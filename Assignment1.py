66e75331cfe1c733751da809ca0f1eef632b1faa
unknown <Shriyashukla93@outlook.com>
2022-09-11 20:53:04 +0530

    Assignment upload

---
+++ Assignment1.py
@@ -0,0 +1,54 @@
+                                                            # Assignment-1: Warm-up with python
+
+# Question 1: Write a command to get the Python version you are using.
+print("output of Question 1")
+import sys
+print(sys.version)
+
+# Question 2: Write a python script to print Hello Python on the screen directly in the command line.
+print("Hello Python")
+
+# Question 3: Write a python script and store it in a file and execute the file from the command line.
+print("python Demo.py")
+# Question 4: Write a python script to print Hello Python on the screen using visual studio code.
+print("output of Question 4")
+print("Hello Python")
+
+# Question 5:  Write a python script to print Hello on the first line and Python on the second line
+print("output of Question 5")
+print("Hello","python",sep='\n')     
+
+# Question 6: Write a python script to print “MySirG” on the screen
+print("output of Question 6")
+print("\"MySirg\"")
+
+# Question 7:  Write a python script to print “Teacher’s Day” on the screen
+print("output of Question 7")
+print("\"Teacher's Day\"")
+
+# different way to do this
+print('''"Teacher's Day"''')
+
+# Question 8:  Write a python script to print the value of a variable. Variable contains your name as data.
+print("output of Question 8")
+Name = 'Shriya Shukla'
+print(Name)
+
+# Question 9: Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+print("output of Question 9")
+Name = 'Shriya Shukla'
+Age  = 22
+Qualification = "B.Tech"
+yrs_of_exp = 2
+
+print("My name:", Name)
+print("My age:", Age)
+print("My qualification:",Qualification)
+print("My Experince:", yrs_of_exp)
+
+# Question 10: Write a python script to print values of three variables, each in a new line. Variable contains integer value
+print("output of Question 10")
+A = 10
+B = 20
+C = 30
+print(A,B,C,sep='\n')
